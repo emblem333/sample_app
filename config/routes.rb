f69c4476e84595d7d70f064644431a7d217c5594
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-02-06 06:46:10 +0000

    [Add]3-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'lists/new'
   get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  #.../lists/1や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy', ad: 'destroy_list'
+  resources :lists
+  # get 'lists/new'
+  # post 'lists' => 'lists#create'
+  # get 'lists' => 'lists#index'
+  # #.../lists/1や .../lists/3 に該当する
+  # get 'lists/:id' => 'lists#show', as: 'list'
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  # patch 'lists/:id' => 'lists#update', as: 'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
 end
